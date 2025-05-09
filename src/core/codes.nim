c7af43cf2a07e80d4f624f1b90ec0ddb430812a1
Andi Rosca <andi23rosca@gmail.com>
2025-05-09 08:34:08 +0200

    feat: Tokenizing

---
+++ src/core/codes.nim
@@ -0,0 +1,5 @@
+const codeBOM* = 0xFEFF
+const codeNull* = 0x0000
+const codeCR* = 0x000D
+const codeLF* = 0x000A
+const codeHT* = 0x0009
--- src/nimcremental_markdown.nim
+++ src/nimcremental_markdown.nim
@@ -1,100 +1,112 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-import strutils
-import nimcremental_markdown/submodule
+import strformat, strutils, unicode, math
+import core/constants, core/codes
 
+type TokenKind* = enum
+  tkBOM # Byte order mark
+  tkVSpace # Virtual space
+  tkEOL # End of line
+  tkEOF # End of file
+  tkCR # Carriage return
+  tkLF # Line feed
+  tkCRLF # Carriage return line feed
+  tkHT # Horizontal tab
+  tkRC # Replacement character
+  tkText
 
-type BlockKind* = enum
-  bkParagraph
-  bkHeading1
-  bkUnorderedList
-  bkOrderedList
-  bkListItem
-  bkIndentedCodeBlock
-  bkFencedCodeBlock
-  bkBlockQuote
-
-type Block* = ref object
-  kind*: BlockKind
-  children*: seq[Block]
-  lineStart*: int
-  lineEnd*: int
+# Only the text token has a proper start and length
+type Token* = object
   offset*: int
-  length*: int
-  open*: bool
-  leaf*: bool
+  kind*: TokenKind
 
-type Document* = ref object
-  children*: seq[Block]
 
-type Parser* = ref object
+type Tokenizer* = object
+  column*: int
+  offset*: int
+  textOffset*: int
   input*: string
-  pos*: int
-  line*: int
-  doc*: Document
-  lastBlock*: Block
+  chunks*: seq[Token]
+
+proc newTokenizer*(): Tokenizer =
+  Tokenizer(
+    column: 1,
+    input: "",
+    chunks: @[],
+    offset: 0,
+    textOffset: 0
+  )
+
+### Looks for the first occurrence of a null, CR, LF, or HT character.
+### Returns the index of the character if found, otherwise -1.
+proc search*(input: string, startPosition: int): int =
+  let length = input.runeLen
+  var i = startPosition
+  while i < length:
+    let r = input.runeAt(i).ord
+    if r == codeNull or r == codeCR or r == codeLF or r == codeHT:
+      return i
+    i += 1
+  if i == length:
+    return -1
+  else:
+    return i
+
+proc pushToken*(tokenizer: var Tokenizer, kind: TokenKind, offset: int) =
+  tokenizer.chunks.add(Token(offset: offset, kind: kind))
 
 
-proc newParser*(): Parser =
-  var doc = Document(children: @[])
-  Parser(input: "", pos: 0, line: 0, doc: doc)
+proc tokenize*(t: var Tokenizer, input: string, finish: bool = false) =
+  t.input = t.input & input
+  let runesInput = input.toRunes()
+  for partialIndex, r in runesInput:
+    let i = t.offset + partialIndex
 
-proc parseBlockContinuation*(parser: var Parser) =
-  echo "parseBlockContinuation"
+    if i == 0 and r.ord == codeBOM:
+      t.pushToken(tkBOM, 0)
+      t.textOffset = 1
+      continue
+    
 
-proc parseBlockStart*(parser: var Parser) =
-  echo "parseBlockStart"
+    case r.ord:
+    of codeCR:
+      if t.textOffset < i:
+        t.pushToken(tkText, t.textOffset)
+        t.textOffset = i + 1
 
-proc debugString*(str: string): string =
-  str.replace("\n", "\\n").replace("\r", "\\r").replace("\r\n", "\\r\\n")
+      t.pushToken(tkCR, i)
+      continue
+    of codeLF:
+      if t.textOffset < i:
+        t.pushToken(tkText, t.textOffset)
+        t.textOffset = i + 1
 
-proc isNewlineToken*(parser: var Parser, i: int): int =
-  if parser.input[i] == '\n':
-    return 1
-  if parser.input[i] == '\r':
-    if i + 1 < parser.input.len and parser.input[i + 1] == '\n':
-      return 2
-    return 1
-  return 0
+      if i > 0 and t.chunks[t.chunks.len - 1].kind == tkCR:
+        t.chunks[t.chunks.len - 1].kind = tkCRLF
+      else:
+        t.pushToken(tkLF, i)
+      continue
+    of codeHT:
+      if t.textOffset < i:
+        t.pushToken(tkText, t.textOffset)
+        t.textOffset = i + 1
+      let next = int(ceil(t.column / tabSize)) * tabSize
+      t.pushToken(tkHT, i)
+      t.column += 1
+      while t.column < next:
+        t.pushToken(tkVSpace, i)
+        t.column += 1
+      continue
+    else:
+      discard
 
-type LineResult = object
-  start: int
-  finish: int
-  newline: bool
-proc newLineResult*(start: int, finish: int, newline: bool): LineResult =
-  LineResult(start: start, finish: finish, newline: newline)
+  t.offset += runesInput.len
 
-proc getLines*(parser: var Parser): seq[LineResult] =
-  var i = parser.pos
-  while i < parser.input.len:
-    var newline = isNewlineToken(parser, i)
-    if newline > 0:
-      if i != parser.pos:
-        result.add(newLineResult(parser.pos, i - 1, false))
-        # echo parser.input[parser.pos..i - 1].debugString
-      result.add(newLineResult(i, i + newline - 1, true))
-      # echo parser.input[i..i + newline - 1].debugString
-      parser.pos = i + newline
-    i += newline + 1
-  
-  if parser.pos != i:
-    result.add(newLineResult(parser.pos, parser.input.len - 1, false))
-    # echo parser.input[parser.pos..parser.input.len - 1].debugString
-    parser.pos = parser.input.len
-
-proc push*(parser: var Parser, input: string) =
-  parser.input.add(input)
-  # echo input.debugString
-
-  let lines = parser.getLines()
-
-
-  parser.parseBlockContinuation()
-  parser.parseBlockStart()
-
+  if finish:
+    echo t.textOffset, " ", t.offset
+    if t.textOffset < t.offset:
+      t.pushToken(tkText, t.textOffset)
+  t.pushToken(tkEOF, t.offset)
 
 when isMainModule:
-  var p = newParser()
-  p.push("# Hell")
-  p.push("o\nit works")
-  # echo(p.input)
+  var tokenizer = newTokenizer()
+  tokenize(tokenizer, "\uFEFFHello\nworld", true)
+  echo tokenizer.chunks
